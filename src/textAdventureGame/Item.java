82a3de0af0cf1494b908b012e5755033f06b7cd1
Prayag <prayagpatel283@gmail.com>
2023-04-11 09:13:39 -0400

    Completed the inventory logic

--- src/textAdventureGame/Item.java
+++ src/textAdventureGame/Item.java
@@ -1,5 +1,24 @@
 package textAdventureGame;
-
 public class Item {
 
+	//Instance Variable.
+	static String notebook = ""; 
+	static String pearl = ""; 
+	
+	Item (String item){
+		switch (item){
+		case "Notebook":
+			notebook = item; 
+			break;
+		}
+	}
+	
+	
+	@Override
+	public String toString() { 
+		String s = "";
+		if(notebook.equals("notebook")) s = notebook; 
+		if(pearl.equals("pearl")) s = pearl; 
+		return s; 
+	}
 }
--- src/textAdventureGame/Room.java
+++ src/textAdventureGame/Room.java
@@ -9,15 +9,12 @@ class Room {
 	private String description;	
 	private String N,S,E,W;
 	
-	// items in this room FIXME (should this be private?)
-	ArrayList<Item> itemList = new ArrayList<Item>();
-	
 	Room(String t, String d) {
 		title = t;
 		description = d;
 	}
 	
-	//FIXME should this be private?
+
 	private void setExits(String N, String S, String W, String E) {
 		this.N = N;
 		this.S = S;
@@ -40,20 +37,37 @@ class Room {
 	
 	//ONLY done at the beginning of the game
 	static void setupRooms(HashMap<String,Room> roomList) {
+		//ROOM #1
 		Room r = new Room("Back yard", "The garden is peaceful with flowers, birds");
 		//          N S W E
 		r.setExits("", "park", "den", "kitchen");
-		roomList.put("backyard", r);
+		roomList.put("backyard", r); 
 		
+		//ROOM #2
 		r = new Room("The Kitchen", "You see a fridge, stove, cubpoards. "
 				+ "The hallway is to the west, east takes you to the back yard");
 		r.setExits("", "", "hall1", "backyard");
 		roomList.put("kitchen", r);
 		
+		//ROOM #3
 		r = new Room("Hallway", "The front hallway connects the front door to the kitchen, living room,"
 				+ " bedrooms (south) and den (down)"); 
 		r.setExits("frontYard", "bedroom1", "livingroom", "kitchen");
 		roomList.put("hall1", r);
+		
+	}
+	
+	//A method that will setup the items for each room. 
+	static void setupItems(ArrayList<String> itemList) {
+		//Items for room 1
+		itemList.add("notebook"); 
+		
+		//Items for room 2
+		itemList.add("cup");
+		
+		//Item for room 3
+		itemList.add("picture");
+		
 	}
 }
   
