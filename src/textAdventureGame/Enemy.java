bc37f31f03b0ce0208e01d9b16372548434d6c2d
Zaid <hasszaid360@gmail.com>
2023-04-15 11:47:49 -0400

    updated the enemy class

--- src/textAdventureGame/Enemy.java
+++ src/textAdventureGame/Enemy.java
@@ -1,5 +1,33 @@
 package textAdventureGame;
 
 public class Enemy {
+	private String name;
+	private int health;
+	private int damage;
 
+	public Enemy(String name, int health, int damage) {
+		this.name = name;
+		this.health = health;
+		this.damage = damage;
+	}
+
+	public void attackPlayer(Player player) {
+		player.takeDamage(damage);
+		System.out.println(name + " attacked you for " + damage + " damage!");
+	}
+
+	public boolean isAlive() {
+		return health > 0;
+	}
+
+	public void takeDamage(int damage) {
+		health -= damage;
+		if (!isAlive()) {
+			System.out.println(name + " has been defeated!");
+		}
+	}
+
+	public int getHealth() {
+		return health;
+	}
 }
