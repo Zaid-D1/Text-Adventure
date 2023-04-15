2de0fb016637e051b15923f02685ea2ee805c437
Zaid <hasszaid360@gmail.com>
2023-04-15 14:22:37 -0400

    added new rooms for the bosses

--- src/textAdventureGame/Room.java
+++ src/textAdventureGame/Room.java
@@ -38,29 +38,34 @@ class Room {
 
 	//ONLY done at the beginning of the game
 	static void setupRooms(HashMap<String,Room> roomList) {
-		Room r = new Room("The Ocean", "The vast, and soundless ocean. There's a forrest to east,"
-				+ "\nA Cave to the West, and A Shipwreck all the way at the bottom of the ocean"
-				+ "\nNorth is out of water, if you go north the seagulls will eat you because you are small");
-		//          N E S W
-		r.setExits("", "Kelp forest", "Shipwreck", "Cave");
-		roomList.put("ocean", r);
+	    Room r = new Room("The Ocean", "The vast, and soundless ocean. There's a forrest to east,"
+	            + "\nA Cave to the West, and A Shipwreck all the way at the bottom of the ocean"
+	            + "\nNorth is out of water, if you go north the seagulls will eat you because you are small");
+	    //          N E S W
+	    r.setExits("", "Kelp forest", "Shipwreck", "Cave");
+	    roomList.put("ocean", r);
 
-		r = new Room("The Kelp forrest", "You see giant kelp all around, water proof notebook. "
-				+ "The opeaning to the forrest is to the east, west takes you back to the still waters");
-		r.setExits("", "", "still waters", "ocean");
-		roomList.put("Kelp forest", r);
+	    r = new Room("The Kelp forrest", "You see giant kelp all around, water proof notebook. "
+	            + "The opening to the forest is to the east, south leads to the Boss Room, west takes you back to the still waters");
+	    r.setExits("", "Boss Room", "still waters", "ocean");
+	    roomList.put("Kelp forest", r);
 
-		r = new Room("Shipwreck", "Scary and somehow still in perfect conditon, the sails are torn but everything else is fine,"
-				+ "\nYou see a door on deck leading to the captians room, and one more door leading towards the ships kitchen"
-				+ "\nStill waters (North)"); 
-		r.setExits("still waters", "", "", "");
-		roomList.put("Shipwreck", r);
-		
-		r = new Room("The Cave", "dark and gloomy, open your water proof flashlight to look around"
-				+ "Still waters (east), Kelp forest (east x2)"); 
-		r.setExits("still waters", "Kelp forrest", "", "");
-		roomList.put("Cave", r);
+	    r = new Room("Shipwreck", "Scary and somehow still in perfect conditon, the sails are torn but everything else is fine,"
+	            + "\nYou see a door on deck leading to the captians room, and one more door leading towards the ships kitchen"
+	            + "\nNorth leads to the Boss Room");
+	    r.setExits("Boss Room", "", "", "still waters");
+	    roomList.put("Shipwreck", r);
+
+	    r = new Room("The Cave", "dark and gloomy, open your water proof flashlight to look around"
+	            + "North leads to the Boss Room, east takes you to the Kelp Forest"); 
+	    r.setExits("Boss Room", "Kelp forrest", "", "");
+	    roomList.put("Cave", r);
+
+	    r = new Room("Boss Room", "A large room with an enemy waiting for you");
+	    r.setExits("Cave", "Shipwreck", "Kelp forest", "");
+	    roomList.put("Boss Room", r);
 	}
+
 
 	//A method that will setup the items for each room. 
 	static void setupItems(HashMap<String,Room> roomList, ArrayList<String> itemList) {
